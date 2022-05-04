054489e86161f54445917f8a1fa51cfed581e3e4
Lorand <sandor.lorand93@gmail.com>
2022-05-04 07:34:26 +0300

    Added hero banner and parallax library

---
+++ src/components/Index/HeroBanner.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeroBanner = () => {
+  return (
+    <div className="module logos">
+      <div className="hero-container">
+        <div className="">
+          <webp url="https://scal.io/assets/images/logos/logo-sheet-color-f170637217.jpg" classes="hero-image" alt="Salspot Client Logos" className="">
+            <img className="hero-image" alt="Salspot Client Logos" src="https://scal.io/assets/images/logos/logo-sheet-color-f170637217.jpg">
+            </img>
+          </webp>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,41 @@
-import React from "react"
-import Navbar from "../components/App/Navbar"
-import Footer from "../components/App/Footer"
-import Layout from "../components/App/Layout"
-import OurSolutions from "../components/Index/OurSolutions"
-import OurServices from "../components/Index/OurServices"
-import OurFeatures from "../components/Index/OurFeatures"
-import TeamMember from "../components/Index/TeamMember"
-import RecentProjects from "../components/Index/RecentProjects"
-import Pricing from "../components/Index/Pricing"
-import Testimonials from "../components/Index/Testimonials"
-import Partner from "../components/Index/Partner"
-import ProjectStartArea from "../components/Index/ProjectStartArea"
-import OurBlog from "../components/Index/OurBlog"
+import React from 'react'
+import Navbar from '../components/App/Navbar'
+import Footer from '../components/App/Footer'
+import Layout from '../components/App/Layout'
+import OurSolutions from '../components/Index/OurSolutions'
+import OurServices from '../components/Index/OurServices'
+import OurFeatures from '../components/Index/OurFeatures'
+import TeamMember from '../components/Index/TeamMember'
+import RecentProjects from '../components/Index/RecentProjects'
+import Pricing from '../components/Index/Pricing'
+import Testimonials from '../components/Index/Testimonials'
+import Partner from '../components/Index/Partner'
+import ProjectStartArea from '../components/Index/ProjectStartArea'
+import OurBlog from '../components/Index/OurBlog'
 import VideoBanner from '../components/Index/VideoBanner';
+import HeroBanner from '../components/Index/HeroBanner';
 
 const Home = () => {
-  return (
-    <Layout>
-      <Navbar />
-      <VideoBanner />
-      <OurSolutions />
-      <OurServices />
-      <OurFeatures />
-      <TeamMember />
-      <RecentProjects />
-      <Pricing />
-      <Testimonials />
-      <Partner />
-      <OurBlog />
-      <ProjectStartArea />
-      <Footer />
-    </Layout>
-  )
+    return (
+        <Layout>
+            <Navbar/>
+            <VideoBanner/>
+            <HeroBanner/>
+
+
+            <OurSolutions/>
+            <OurServices/>
+            <OurFeatures/>
+            <TeamMember/>
+            <RecentProjects/>
+            <Pricing/>
+            <Testimonials/>
+            <Partner/>
+            <OurBlog/>
+            <ProjectStartArea/>
+            <Footer/>
+        </Layout>
+    )
 }
 
 export default Home
